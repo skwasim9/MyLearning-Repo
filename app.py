641cd9651a2d126fc3fb4c0153567f3486702116
skwasim9 <119466786+skwasim9@users.noreply.github.com>
2025-11-27 16:15:22 +0800

    Update home route message for deployment success

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Jenkins Docker GitHub Deployment Successful..Wasim Hanif Shaikh "
+    return "Hello! Jenkins Docker GitHub Deployment Successful..Wasim Hanif Shaikh!!"
 
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0", port=8000)
+
 
